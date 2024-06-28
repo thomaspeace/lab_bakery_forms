3368f47e952f7dfbece10b432b40e3d6635a0aae
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-28 15:04:51 +0100

    add step 1 MVP

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,34 @@ import Title from "./Title";
 import RecipeList from "./RecipeList";
 import Search from "./Search";
 import CakeForm from "./CakeForm";
+import { useState } from "react";
 
 function App() {
+
+  const [cakes, setCakes] = useState([
+      {
+          cakeName: "Lemon Drizzle",
+          ingredients: ["eggs", "butter", "lemon  zest", "sugar", "self-raising flour"],
+          rating: 5
+      },
+      {
+          cakeName: "Tea Loaf",
+          ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
+          rating: 3
+      },
+      {
+          cakeName: "Brownie",
+          ingredients: ["chocolate", "eggs", "flour", "butter", "walnuts"],
+          rating: 4
+      },
+      {
+          cakeName: "Carrot Cake",
+          ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
+          rating: 5
+      }
+  ])
+
+
   return (
     <>
       <header>
